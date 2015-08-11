a8562ad6d4115f0bc0fb89d6406dc10bd2c54fb4
Nesurion <maik.schwan@gmail.com>
2015-08-11 18:31:14 +0200

    run compile script on install

---
+++ compile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+npm install grunt
+npm install grunt-cli
+npm install grunt-contrib-coffee
+npm install grunt-contrib-watch
+npm install grunt-release
+npm install grunt-shell
+grunt coffee
