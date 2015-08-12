840f684c6c77f542c0bf49e4a490f51c168a76ba
Nesurion <maik.schwan@gmail.com>
2015-08-12 10:21:54 +0200

    remove compile script

--- compile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-npm install grunt
-npm install grunt-cli
-npm install grunt-contrib-coffee
-npm install grunt-contrib-watch
-npm install grunt-release
-npm install grunt-shell
-grunt coffee
